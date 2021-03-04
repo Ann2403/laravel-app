d649d1f2f2eadef2164073c91c4ca5ebbdc1d762
Ann <zubenko.job@gmail.com>
2021-03-04 10:49:34 +0200

    Part 1: Configuration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,12 @@ class HomeController extends Controller
 
     public function index()
     {
+        //массив с глобальными переменными среды
+        dump($_ENV);
+        //функция хелпер для просмотра или установки(вторым параметром передается значение) глобальных переменных среды
+        dump(env('DB_DATABASE'));
+        //функция хелпер для просмотра либо установки конфиграций приложения
+        dump(config('app.timezone'));
         //возвращаем представление(страницу) home
         return view('home', ['res' => 'I love you!', 'name' => 'Andrey']);
     }
